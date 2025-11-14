7744f0905d4ce6cab2c914e5a6c05d9a7b334824
nao270374 <270374g@ugs.kochi-tech.ac.jp>
2025-11-14 15:47:59 +0900

    change file mini_max

--- mini_max.py
+++ mini_max.py
@@ -5,4 +5,17 @@ class GameState(Enum):
     DRAW = auto()
     ON = auto()
     OVER = auto()
+
+# management class of board's mark
+class Mark(Enum):
+    X = auto()
+    O = auto()
+    EMPTY = auto()
+
+# class for managing the overall state of the board
+class Board:
+    # initialize board
+    def __init__(self):
+        self.cell = [[Mark.EMPTY for i in range(3)] for j in range(3)]
+        self.is_first_player = True
     
